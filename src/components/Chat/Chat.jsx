ae9d7aad6b0819c72eac2e1b81b385734f2abb8e
Victor Apaez <vic.apaez@gmail.com>
2022-11-21 14:41:06 -0500

    created chat with a search bar

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./Chat.css";
+import { BiMessageRoundedAdd, BiSearch } from "react-icons/bi";
+import { CiMinimize1 } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <div className="chat-container">
+      <div className="chat-aside">
+        <h3 className="chat-aside-title">Chat</h3>
+        <BiMessageRoundedAdd className="chat-new-msg"></BiMessageRoundedAdd>
+      </div>
+
+      <div className="chat-main">
+        <h3 className="chat-main-header">New Chat</h3>
+        <div className="chat-main-icons">
+          <CiMinimize1 className="chat-main-minimize" />
+          <AiOutlineClose />
+        </div>
+
+        <div className="chat-search-container">
+          <p className="chat-search-title">SEARCH</p>
+          <div className="chat-search-input">
+            <BiSearch className="chat-search-icon"></BiSearch>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
