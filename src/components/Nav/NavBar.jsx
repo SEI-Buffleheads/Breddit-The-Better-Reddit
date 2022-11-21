ab06dd28be913770693b94bab16590a0f3ae44e3
“Brandon <balvarez325@gmail.com>
2022-11-21 15:12:42 -0500

    set responsive design

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import {Navbar, Nav} from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { FaRegUserCircle } from "react-icons/fa"
-import { AiOutlineDown } from "react-icons/ai"
-import logo from "../../assets/logos/reddisc.png"
+import {LinkContainer} from "react-router-bootstrap";
+import {FaRegUserCircle} from "react-icons/fa";
+import {AiOutlineDown} from "react-icons/ai";
+import logo from "../../assets/logos/reddisc.png";
 import "./Nav.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg" className="nav-container">
-
       <LinkContainer to="/">
         <Navbar.Brand>
-          <img src={logo} alt="that logo boiii" style={{ height: 25, marginTop: 6}} />
+          <img
+            src={logo}
+            alt="that logo boiii"
+            style={{height: 25}}
+          />
         </Navbar.Brand>
       </LinkContainer>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
       <Navbar.Collapse id="basic-navbar-nav">
-        
         <Nav className="mr-auto">
           <LinkContainer to="/signup">
             <Nav.Link>SignUp</Nav.Link>
