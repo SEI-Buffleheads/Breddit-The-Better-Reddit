423e8d591f38e5009ada2e85a8e630e94c2c8c0f
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-22 16:56:23 -0500

    adjusted width of ads and aside, ready to merge

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,17 @@ import test from "../../images/test-img.png"
 function Home() {
   return (
     <div className="home-container">
-      <div className="nav-component">navvvv</div>
+      <div className="nav-component">Place Nav Component here! :D</div>
       <div className="trending-container">
         <div className="trending-text">Trending Today</div>
         <div className="trending-card-container">
-        <div className="trending-card trending1">trending1</div>
-        <div className="trending-card trending2">trending2</div>
-        <div className="trending-card trending3">trending3</div>
-        <div className="trending-card trending4">trending4</div>
+        <div className="trending-card trending1">Platano</div>
+        <div className="trending-card trending2">Con</div>
+        <div className="trending-card trending3">Salami</div>
+        <div className="trending-card trending4">Y Huevo</div>
       </div>
       </div>
-      <div className="aside">ASIDEEEE</div>
+      <div className="aside">Place Aside Component here! :D</div>
       <div className="main-container">
         <div className="posts">
           <div className="post-cards">
@@ -36,7 +36,11 @@ function Home() {
           <div className="post-cards"></div>
           <div className="post-cards"></div>
         </div>
-        <div className="ads">I can't skip the ads</div>
+        <div className="ads">I can't skip the ads
+          <div className="ad1">
+            <img alt="" src={ test }/>
+          </div>
+        </div>
       </div>
     </div>
   );
