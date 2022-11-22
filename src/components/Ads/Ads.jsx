fde326d13cc14859bfcb51780ec6d53fc33aa953
kapatric <khristopherpatrick@gmail.com>
2022-11-22 09:09:04 -0500

    added href

--- src/components/Ads/Ads.jsx
+++ src/components/Ads/Ads.jsx
@@ -7,23 +7,23 @@ function Ads() {
   return(
     <div className="ads-aside">
       <aside >
-        <div><img className="ads" src="https://i.pinimg.com/originals/b0/36/40/b036409e7db643848e86858d55edd26d.png" /></div>
-        <div><img className="ads" src="https://www.gourmetads.com/wp-content/uploads/2019/05/fast-food-ads-mcdonalds-300x600.jpg" /></div>
-        <div><img className="ads" src="https://habitatfwb.org/wp-content/uploads/2019/09/300x600-banner-ad.jpg" /></div>
+        <div><img className="ads" src="https://i.pinimg.com/originals/b0/36/40/b036409e7db643848e86858d55edd26d.png" alt=""/></div>
+        <div><img className="ads" src="https://www.gourmetads.com/wp-content/uploads/2019/05/fast-food-ads-mcdonalds-300x600.jpg" alt=""/></div>
+        <div><img className="ads" src="https://habitatfwb.org/wp-content/uploads/2019/09/300x600-banner-ad.jpg" alt="" /></div>
       </aside>
       <div className="reddit-inc">
        <div><a href="https://www.redditinc.com/policies/user-agreement">User Agreement</a>
-        <a >Privacy Policy</a>
-        <a>Content Policy</a>
-          <a>Moderator Code of Conduct</a>
+        <a href="https://www.reddit.com/policies/privacy-policy">Privacy Policy</a>
+        <a href="https://www.redditinc.com/policies/content-policy">Content Policy</a>
+          <a href="#">Moderator Code of Conduct</a>
         </div> 
         <div>
-          <a>English</a>
-          <a>Français</a>
-          <a>Italiano</a>
-          <a>Deutsch</a>
-          <a>Español</a>
-          <a>Portugués</a>
+          <a href="#">English</a>
+          <a href="#">Français</a>
+          <a href="#">Italiano</a>
+          <a href="#">Deutsch</a>
+          <a href="#">Español</a>
+          <a href="#">Portugués</a>
         </div>
       </div>
       <div>
