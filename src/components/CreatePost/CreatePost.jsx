a0787a70b1b69909526248cbe20c2e56cfdb0436
erinbaright <erin.baright29@gmail.com>
2022-11-22 11:15:31 -0500

    create a post page basic layout looking good

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -3,7 +3,29 @@ import "./CreatePost.css"
 
 function CreatePost() {
   return (
-    <div>Hi</div>
+    <div className="create-post-container">
+
+      <h3 className="create-a-post">Create a post</h3>
+      <hr id="line"></hr>
+
+      <div className="post-details">
+        <div className="input-flex">
+          <input type="text" id="post-title" placeholder="Title"/>
+          <input type="text" id="post-text" placeholder="Text (optional)" />
+        </div>
+        
+        <div className="file-flex">
+          <form action="/url" method="GET">
+            <input type="file" id="file-button"/>
+          </form>
+        </div>
+
+        <div className="post-button-flex">
+          <button id="post-button">Post</button>
+        </div>  
+      </div>
+
+    </div>
     )
   }
   
