df24f0de5c16f71f7024f7e342292b60e5e10a19
Nichole De La Rosa <nicholedlrosa@gmail.com>
2022-11-22 16:44:43 -0500

    routes added to switch tabs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
 import User from "./pages/User/User.jsx";
-import UserTabs from "./pages/User/All-Tabs/UserTabs.js";
+import UserTabs from "./pages/User/All-Tabs/UserTabs.jsx";
 import Chat from "./components/Chat/Chat.jsx";
 
 import "./App.css";
@@ -15,7 +15,6 @@ function App() {
     <div className="App">
       {/* <Chat /> */}
       <User />
-      <UserTabs />
     </div>
   );
 }
--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,29 +1,42 @@
-import React from 'react';
-import { link } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import UserTabs from "./All-Tabs/UserTabs.jsx";
+import UserTab1 from "./All-Tabs/userTab1.jsx";
+import UserTab2 from "./All-Tabs/userTab2.jsx";
+import UserTab3 from "./All-Tabs/userTab3.jsx";
+import UserTab4 from "./All-Tabs/userTab4.jsx";
+import UserTab5 from "./All-Tabs/userTab5.jsx";
+import UserTab6 from "./All-Tabs/userTab6.jsx";
+import UserTab7 from "./All-Tabs/userTab7.jsx";
+import UserTab8 from "./All-Tabs/userTab8.jsx";
+import UserTab9 from "./All-Tabs/userTab9.jsx";
 import "./User.css";
+// import props from handleClick
 
- 
- function User() {
-   return (
-     <header>
-       <nav>
-         {/*
-       <link to='/'> home </link>
-          <link to='/userTab1'> tab1 </link>
-         <link to='/userTab2'> tab2 </link>
-         <link to='/userTab3'> tab3 </link>
-         <link to='/userTab4'> tab4 </link>
-         <link to='/userTab5'> tab5 </link>
-         <link to='/userTab6'> tab6 </link>
-         <link to='/userTab7'> tab7 </link> 
-         <link to='/userTab8'> tab8 </link>
-         <link to='/userTab9'> tab9 </link> 
-         */}
-         
-       </nav>
-       </header>
-    
-   )
- }
- 
- export default User
+function User() {
+  return (
+    <>
+      <div className="user-pg">
+        <div className="UserTabs">
+          <UserTabs />
+
+          <div className="user-outlet">
+            <Routes>
+              <Route path="/user-feed/overview" element={<UserTab1 />} />
+              <Route path="/user-feed/posts" element={<UserTab2 />} />
+              <Route path="/user-feed/comments" element={<UserTab3 />} />
+              <Route path="/user-feed/history" element={<UserTab4 />} />
+              <Route path="/user-feed/saved" element={<UserTab5 />} />
+              <Route path="/user-feed/hidden" element={<UserTab6 />} />
+              <Route path="/user-feed/upvote" element={<UserTab7 />} />
+              <Route path="/user-feed/downvote" element={<UserTab8 />} />
+              <Route path="/user-feed/awards" element={<UserTab9 />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default User;
