65fb687ef8358401a8d03a81db206ee2a454b6ef
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-11-22 13:54:51 -0500

    moved trending container to the top, made posts infinite scroll, aside bar ready

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Home.css";
+import test from "../../images/test-img.png"
 
 function Home() {
   return (
     <div className="home-container">
-      <div className="trending-component">
-        <div className="trending-cards trending1">trending1</div>
-        <div className="trending-cards trending2">trending2</div>
-        <div className="trending-cards trending3">trending3</div>
-        <div className="trending-cards trending4">trending4</div>
+      <div className="nav-component">navvvv</div>
+      <div className="trending-container">
+        <div className="trending-text">Trending Today</div>
+        <div className="trending-card-container">
+        <div className="trending-card trending1">trending1</div>
+        <div className="trending-card trending2">trending2</div>
+        <div className="trending-card trending3">trending3</div>
+        <div className="trending-card trending4">trending4</div>
+      </div>
       </div>
       <div className="aside">ASIDEEEE</div>
       <div className="main-container">
         <div className="posts">
+          <div className="post-cards">
+            <div className="post-content">
+              <img alt="" src={ test } />
+          </div>
+          </div>
+          <div className="post-cards"></div>
+          <div className="post-cards"></div>
           <div className="post-cards"></div>
           <div className="post-cards"></div>
           <div className="post-cards"></div>
