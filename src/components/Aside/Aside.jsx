4b4c9ae0572d8e95670a6ccf09d8cda32fa8dfd8
Brandon-Alvarez-03 <100237632+Brandon-Alvarez-03@users.noreply.github.com>
2022-11-28 11:16:06 -0500

    add initial icons and flex styling

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,11 +2,38 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Aside.css";
 import {GoHome} from "react-icons/go";
+import {
+  GiPartyPopper,
+  GiBalloonDog,
+  GiSlicedBread,
+  GiGingerbreadMan,
+} from "react-icons/gi";
 
 function Aside() {
   return (
     <div className="aside">
-      <GoHome size={22} className="aside-icon" />
+      <div className="aside-link-wrapper">
+      <div className="aside-link-container">
+        <GoHome size={24} className="aside-icon" />
+        <p>Home</p>
+      </div>
+      <div className="aside-link-container">
+        <GiPartyPopper size={25} className="aside-icon" />
+        <p>Popular</p>
+      </div>
+      <div className="aside-link-container">
+        <GiBalloonDog size={25} className="aside-icon" />
+        <p>All</p>
+      </div>
+      <div className="aside-link-container">
+        <GiSlicedBread size={25} className="aside-icon" />
+        <p>Happening</p>
+      </div>
+      <div className="aside-link-container">
+        <GiGingerbreadMan size={25} className="aside-icon" />
+        <p>Communities</p>
+        </div>
+        </div>
     </div>
   );
 }
