d3c2195740ff66cff257e7818527aad4364e7c28
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-28 12:08:10 -0500

    styled new signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,54 @@
-import React from "react";
 import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import { signIn } from "../../services/user";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 function Login() {
+  const { dispatch } = useAuthContext();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const form = {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value,
+      };
+      const user = await signIn(form);
+
+      if (user.name === "AxiosError") {
+        alert("Invalid Username or password");
+      } else {
+        dispatch({ type: "LOGIN", payload: user });
+        navigate("/", { replace: true });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="login-container">
-      <form className="login-form">
-        <h2 className="login-header">Log In</h2>
+    <div className="signIn">
+      <form className="signForm" onSubmit={handleSubmit}>
         <input
-          className="login-input login-username"
+          className="input"
+          type="text"
           placeholder="Enter Username"
           name="username"
+          ref={usernameRef}
         />
         <input
-          className="login-input login-password"
+          className="input"
           type="password"
           placeholder="Enter Password"
           name="password"
+          ref={passwordRef}
         />
-        <button className="login-input login-submit" type="submit">
-          Submit
+        <button type="submit" className="subBtn">
+          Sign In
         </button>
       </form>
     </div>
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,48 +1,71 @@
-import React from "react";
 import "./Signup.css";
+import { useRef } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { signUp } from "../../services/user";
+import { useNavigate } from "react-router-dom";
 
-function Signup() {
+const Signup = () => {
+  const { dispatch, user } = useAuthContext();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const confirmRef = useRef();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      passwordRef.current.value !== confirmRef.current.value ||
+      passwordRef.current.value.length < 8
+    ) {
+      alert("Please use a valid password.");
+    } else {
+      try {
+        const form = {
+          username: usernameRef.current.value,
+          password: passwordRef.current.value,
+        };
+        const user = await signUp(form);
+        dispatch({ type: "LOGIN", payload: user });
+        navigate("/", { replace: true });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
   return (
-    <div className="signup-container">
-      <form className="signup-form">
-        <h2 className="signup-header">Sign Up</h2>
-
+    <form onSubmit={handleSubmit} className="signup-container">
+      <div className="signup-form">
+        <h1 className="signup-header">Sign Up</h1>
         <input
-          className="signup-username signup-input"
           type="text"
           placeholder="Enter Username"
           name="username"
+          ref={usernameRef}
+          className="signup-input"
         />
-
         <input
-          className="signup-email signup-input"
-          type="email"
-          placeholder="Enter Email"
-          name="email"
-        />
-
-        <input
-          className="signup-password signup-input"
+          className="signup-input"
           type="password"
           minLength="8"
           placeholder="Enter Password"
           name="password"
+          ref={passwordRef}
           pattern="(?=.*\d)(?=.*[a-z])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><]).{8,}"
         />
 
         <input
-          className="signup-confirm-password signup-input"
+          className="signup-input"
           type="password"
+          ref={confirmRef}
           name="passwordConfirm"
           placeholder="Confirm Password"
         />
-
-        <button className="signup-submit signup-input" type="submit">
+        <button className="signup-submit" type="submit">
           Create Account
         </button>
-      </form>
-    </div>
+      </div>
+    </form>
   );
-}
+};
 
 export default Signup;
