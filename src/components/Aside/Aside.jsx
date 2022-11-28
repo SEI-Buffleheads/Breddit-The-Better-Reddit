a99b20e7f44f51c06ee17744365d8a0bef8dd130
Brandon-Alvarez-03 <100237632+Brandon-Alvarez-03@users.noreply.github.com>
2022-11-28 11:55:40 -0500

    aside icns and styling set/responsive

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Aside.css";
-import {GoHome} from "react-icons/go";
+import { GoHome } from "react-icons/go";
 import {
   GiPartyPopper,
   GiBalloonDog,
   GiSlicedBread,
   GiGingerbreadMan,
+  GiBread
 } from "react-icons/gi";
+import { RiGameLine } from "react-icons/ri"
+import {
+  MdOutlineSportsFootball,
+  MdOutlineFastfood,
+  MdOutlineMoreHoriz,
+} from "react-icons/md";
+import {TbBusinessplan} from "react-icons/tb"
 
 function Aside() {
   return (
@@ -27,7 +35,7 @@ function Aside() {
           <p>All</p>
         </div>
         <div className="aside-link-container">
-          <GiSlicedBread size={25} className="aside-icon" />
+          <GiBread size={25} className="aside-icon" />
           <p>Happening</p>
         </div>
         <div className="aside-link-container">
@@ -38,15 +46,15 @@ function Aside() {
       <div className="aside-link-wrapper">
         <p className="aside-wrapper-text">Topics</p>
         <div className="aside-link-container">
-          <GoHome size={24} className="aside-icon" />
+          <RiGameLine size={24} className="aside-icon" />
           <p>Gaming</p>
         </div>
         <div className="aside-link-container">
-          <GiPartyPopper size={25} className="aside-icon" />
+          <MdOutlineSportsFootball size={25} className="aside-icon" />
           <p>Sports</p>
         </div>
         <div className="aside-link-container">
-          <GiBalloonDog size={25} className="aside-icon" />
+          <TbBusinessplan size={25} className="aside-icon" />
           <p>Business</p>
         </div>
         <div className="aside-link-container">
@@ -54,11 +62,11 @@ function Aside() {
           <p>Crypto</p>
         </div>
         <div className="aside-link-container">
-          <GiGingerbreadMan size={25} className="aside-icon" />
-          <p>Television</p>
+          <MdOutlineFastfood size={25} className="aside-icon" />
+          <p>Food</p>
         </div>
         <div className="aside-link-container">
-          <GiGingerbreadMan size={25} className="aside-icon" />
+          <MdOutlineMoreHoriz size={25} className="aside-icon" />
           <p>More Topics</p>
         </div>
       </div>
