ee28eb202862ef87f6bb85bba15ebcb2741d2552
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-28 15:18:03 -0500

    centered signup/login

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -33,38 +33,40 @@ const Signup = () => {
     }
   };
   return (
-    <form onSubmit={handleSubmit} className="signup-container">
-      <div className="signup-form">
-        <h1 className="signup-header">Sign Up</h1>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          name="username"
-          ref={usernameRef}
-          className="signup-input"
-        />
-        <input
-          className="signup-input"
-          type="password"
-          minLength="8"
-          placeholder="Enter Password"
-          name="password"
-          ref={passwordRef}
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><]).{8,}"
-        />
+    <div className="signup-center">
+      <form onSubmit={handleSubmit} className="signup-container">
+        <div className="signup-form">
+          <h1 className="signup-header">Sign Up</h1>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            ref={usernameRef}
+            className="signup-input"
+          />
+          <input
+            className="signup-input"
+            type="password"
+            minLength="8"
+            placeholder="Enter Password"
+            name="password"
+            ref={passwordRef}
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><]).{8,}"
+          />
 
-        <input
-          className="signup-input"
-          type="password"
-          ref={confirmRef}
-          name="passwordConfirm"
-          placeholder="Confirm Password"
-        />
-        <button className="signup-submit" type="submit">
-          Create Account
-        </button>
-      </div>
-    </form>
+          <input
+            className="signup-input"
+            type="password"
+            ref={confirmRef}
+            name="passwordConfirm"
+            placeholder="Confirm Password"
+          />
+          <button className="signup-submit" type="submit">
+            Create Account
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
