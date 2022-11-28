39f19c4f938c652c4ed8c986771352a6d060e5de
Brandon-Alvarez-03 <100237632+Brandon-Alvarez-03@users.noreply.github.com>
2022-11-28 11:31:17 -0500

    update aside topics

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -13,27 +13,55 @@ function Aside() {
   return (
     <div className="aside">
       <div className="aside-link-wrapper">
-      <div className="aside-link-container">
-        <GoHome size={24} className="aside-icon" />
-        <p>Home</p>
-      </div>
-      <div className="aside-link-container">
-        <GiPartyPopper size={25} className="aside-icon" />
-        <p>Popular</p>
-      </div>
-      <div className="aside-link-container">
-        <GiBalloonDog size={25} className="aside-icon" />
-        <p>All</p>
-      </div>
-      <div className="aside-link-container">
-        <GiSlicedBread size={25} className="aside-icon" />
-        <p>Happening</p>
-      </div>
-      <div className="aside-link-container">
-        <GiGingerbreadMan size={25} className="aside-icon" />
-        <p>Communities</p>
+        <p className="aside-wrapper-text">Feeds</p>
+        <div className="aside-link-container">
+          <GoHome size={24} className="aside-icon" />
+          <p>Home</p>
         </div>
+        <div className="aside-link-container">
+          <GiPartyPopper size={25} className="aside-icon" />
+          <p>Popular</p>
         </div>
+        <div className="aside-link-container">
+          <GiBalloonDog size={25} className="aside-icon" />
+          <p>All</p>
+        </div>
+        <div className="aside-link-container">
+          <GiSlicedBread size={25} className="aside-icon" />
+          <p>Happening</p>
+        </div>
+        <div className="aside-link-container">
+          <GiGingerbreadMan size={25} className="aside-icon" />
+          <p>Communities</p>
+        </div>
+      </div>
+      <div className="aside-link-wrapper">
+        <p className="aside-wrapper-text">Topics</p>
+        <div className="aside-link-container">
+          <GoHome size={24} className="aside-icon" />
+          <p>Gaming</p>
+        </div>
+        <div className="aside-link-container">
+          <GiPartyPopper size={25} className="aside-icon" />
+          <p>Sports</p>
+        </div>
+        <div className="aside-link-container">
+          <GiBalloonDog size={25} className="aside-icon" />
+          <p>Business</p>
+        </div>
+        <div className="aside-link-container">
+          <GiSlicedBread size={25} className="aside-icon" />
+          <p>Crypto</p>
+        </div>
+        <div className="aside-link-container">
+          <GiGingerbreadMan size={25} className="aside-icon" />
+          <p>Television</p>
+        </div>
+        <div className="aside-link-container">
+          <GiGingerbreadMan size={25} className="aside-icon" />
+          <p>More Topics</p>
+        </div>
+      </div>
     </div>
   );
 }
