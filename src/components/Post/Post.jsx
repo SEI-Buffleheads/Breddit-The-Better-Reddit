c9d68dd05e4a62b10d58403de2582ada00b0d6a5
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-11-29 16:28:21 -0500

    default links and category

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Comments from "../Comments/Comments.jsx";
-import "./Post.css";
-import { BsArrowUpSquare } from "react-icons/bs";
-import { BsArrowDownSquare } from "react-icons/bs";
-import { getPost } from "../../services/Post.js";
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import Comments from "../Comments/Comments.jsx";
+// import "./Post.css";
+// import { BsArrowUpSquare } from "react-icons/bs";
+// import { BsArrowDownSquare } from "react-icons/bs";
+// import { getPost } from "../../services/Post.js";
 
 
-function Post() {
+// function Post() {
 
-  return (
-    <div className="post-details">
+//   return (
+//     <div className="post-details">
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default Post
+// export default Post
