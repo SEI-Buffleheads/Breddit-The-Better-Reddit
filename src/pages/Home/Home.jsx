edfdd5751597d8a2cbe9851d1386e16c6dc3eb41
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-29 15:02:44 -0500

    adjusted home screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./Home.css";
 
 import test from "../../images/test-img.png";
-import Post from "../../components/Posts/Posts";
+import Posts from "../../components/Posts/Posts";
 import Ads from "../../components/Ads/Ads";
 
 function Home() {
   return (
     <div className="home-container">
-      <Post className="home-posts" />
+      <Posts className="home-posts" />
       <Ads className="home-ads" />
     </div>
   );
