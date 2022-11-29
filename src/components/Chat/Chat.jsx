e399cb58a64230f1326a5f78902e1f7925f150be
Victor Apaez <vic.apaez@gmail.com>
2022-11-29 15:13:51 -0500

    imported getUsers service and displaying their usernames

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,10 +1,25 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Chat.css";
 import { BiMessageRoundedAdd, BiSearch } from "react-icons/bi";
 import { CiMinimize1 } from "react-icons/ci";
 import { AiOutlineClose } from "react-icons/ai";
+import { getUsers } from "../../services/user";
+import InputUsers from "./InputUsers";
 
 function Chat({ setToggleChat, setShowChat }) {
+  const [users, setUsers] = useState(null);
+  const [recipients, setRecipients] = useState([]);
+  const [usernameInput, setUsernameInput] = useState("");
+
+  useEffect(() => {
+    async function getAllUsers() {
+      let users = await getUsers();
+      setUsers(users);
+      console.log(users);
+    }
+    getAllUsers();
+  }, []);
+
   function handleMinimize() {
     setToggleChat(true);
   }
@@ -12,6 +27,16 @@ function Chat({ setToggleChat, setShowChat }) {
     setShowChat(false);
   }
 
+  function checkForUser() {
+    users.forEach((user) => {
+      console.log(user.username.toLowerCase().includes(usernameInput));
+    });
+  }
+
+  function handleUsernameInput(e) {
+    setUsernameInput(e.target.value);
+    checkForUser();
+  }
   return (
     <div className="chat-container">
       <div className="chat-aside">
@@ -37,13 +62,21 @@ function Chat({ setToggleChat, setShowChat }) {
           <p className="chat-search-title">SEARCH</p>
           <div className="chat-search-input-wrapper">
             <div className="chat-search-input-field">
-              <BiSearch className="chat-search-icon"></BiSearch>
+              {/* <BiSearch className="chat-search-icon"></BiSearch> */}
+              {recipients.map((recipient) => {
+                return <span>{recipient}</span>;
+              })}
+              <span>Victor</span>
               <input
+                value={usernameInput}
+                name="usernameInput"
                 className="chat-search-input"
                 type="text"
                 placeholder="Type usernames"
+                onChange={(e) => handleUsernameInput(e)}
               />
             </div>
+            <InputUsers users={users} />
           </div>
         </div>
       </div>
