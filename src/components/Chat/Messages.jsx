b72b32bc0bba2952bcb17607028c7b00d904ff51
Victor Apaez <vic.apaez@gmail.com>
2022-11-30 16:52:39 -0500

    displaying updated messages

--- src/components/Chat/Messages.jsx
+++ src/components/Chat/Messages.jsx
@@ -3,18 +3,20 @@ import { useState } from "react";
 
 function Messages({ currentRoom, sendMessage }) {
   const [msg, setMsg] = useState("");
-
+  console.log(currentRoom);
   return (
     <div className="messages-container">
       <div className="messages-text-container">
         <p>Hey this is a text!</p>
         <p>Hey this another text!</p>
-        <p>{currentRoom.roomId}</p>
+        {currentRoom.messages.map((msg) => {
+          return <p>{msg}</p>;
+        })}
       </div>
 
       <form
         className="messages-form"
-        onSubmit={(e) => sendMessage(e, msg, currentRoom.roomId)}
+        onSubmit={(e) => sendMessage(e, msg, currentRoom)}
       >
         <input value={msg} onChange={(e) => setMsg(e.target.value)} />
         <button>Send</button>
