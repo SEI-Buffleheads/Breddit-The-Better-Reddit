6054b6fcee3e44f56e31f78204a0dc74be1500c9
Victor Apaez <vic.apaez@gmail.com>
2022-11-30 13:58:13 -0500

    changing the currentRoom state when clicking on a room and setting it to null when clicking icon for showing ChatSearch

--- src/components/Chat/ChatAside.jsx
+++ src/components/Chat/ChatAside.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { BiMessageRoundedAdd, BiSearch } from "react-icons/bi";
 
-function ChatAside() {
+function ChatAside({ allRooms, setCurrentRoom }) {
+  function handleRoomClick(room) {
+    setCurrentRoom(room);
+    console.log(room);
+    localStorage.setItem("currentRoom", JSON.stringify(room));
+  }
+  function handleShowCreateGroup() {
+    setCurrentRoom(null);
+  }
+
   return (
     <div className="chat-aside">
       <div className="chat-aside-nav">
         <h3 className="chat-aside-title">Chat</h3>
-        <BiMessageRoundedAdd className="chat-new-msg"></BiMessageRoundedAdd>
+        <BiMessageRoundedAdd
+          className="chat-new-msg"
+          onClick={handleShowCreateGroup}
+        ></BiMessageRoundedAdd>
+      </div>
+      <div className="chat-aside-rooms">
+        {allRooms.map((room) => {
+          return (
+            <div
+              className="chat-aside-room"
+              onClick={() => {
+                handleRoomClick(room);
+              }}
+            >
+              {room.roomId}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
