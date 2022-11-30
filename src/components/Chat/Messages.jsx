d3bb517582ed1a2d8a585367f855a5a7d42590ba
Victor Apaez <vic.apaez@gmail.com>
2022-11-30 17:24:46 -0500

    added chat nav bar to close and minimize chat

--- src/components/Chat/Messages.jsx
+++ src/components/Chat/Messages.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { useState } from "react";
+import { CiMinimize1 } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
 
-function Messages({ currentRoom, sendMessage }) {
+function Messages({ currentRoom, sendMessage, setToggleChat, setShowChat }) {
   const [msg, setMsg] = useState("");
   console.log(currentRoom);
+
+  function handleMinimize() {
+    setToggleChat(true);
+  }
+  function handleClose() {
+    setShowChat(false);
+  }
   return (
     <div className="messages-container">
+      <div className="chat-main-nav">
+        <h3 className="chat-main-title">Chat Name</h3>
+        <div className="chat-main-icons">
+          <CiMinimize1
+            className="chat-main-minimize"
+            onClick={handleMinimize}
+          />
+          <AiOutlineClose onClick={handleClose} />
+        </div>
+      </div>
       <div className="messages-text-container">
         <p>Hey this is a text!</p>
         <p>Hey this another text!</p>
